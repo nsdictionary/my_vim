05518af556bf1f5b2da6b0d39d3fae9d3719039b
sanghyunryu <3161728@naver.com>
2017-06-27 16:57:48 +0900

    Add ideavimrc

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 ln -s ~/Dropbox/sign/my_vim/vimrc ~/.vimrc
 ln -s ~/Dropbox/sign/my_vim/vim ~/.vim
+ln -s ~/Dropbox/sign/my_vim/vim/vimrcs/ideavimrc ~/.ideavimrc
 vim +PluginInstall +qall
 
 echo "Installed the Ultimate Vim configuration successfully! Enjoy :-)"
