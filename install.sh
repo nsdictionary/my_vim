ecb9a583e574a4e0a893d697fe359e1293d9cecb
sam <3161728@naver.com>
2020-04-14 13:58:29 +0900

    Add vrapperrc

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 ln -s ~/Dropbox/sign/my_vim/vimrc ~/.vimrc
 ln -s ~/Dropbox/sign/my_vim/vim ~/.vim
 ln -s ~/Dropbox/sign/my_vim/vim/vimrcs/ideavimrc ~/.ideavimrc
+ln -s ~/Dropbox/sign/my_vim/vim/vimrcs/vrapperrc ~/.vrapperrc
 vim +PluginInstall +qall
 
 echo "Installed the Ultimate Vim configuration successfully! Enjoy :-)"
